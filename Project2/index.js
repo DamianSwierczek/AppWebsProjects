012fef4f86904b77657071c159ad71e5de9cb35d
Damian Świerczek <damian.swierczek@microsoft.wsei.edu.pl>
2021-04-11 23:08:09 +0200

    Working version with channels

--- Project2/index.js
+++ Project2/index.js
@@ -8,16 +8,84 @@ var rideSound;
 var snareSound;
 var boomSound;
 var channel1 = [];
+var channel2 = [];
+var channel3 = [];
+var channel4 = [];
+var ifRecord1 = false;
+var timeRecord1 = 0;
+var ifRecord2 = false;
+var timeRecord2 = 0;
+var ifRecord3 = false;
+var timeRecord3 = 0;
+var ifRecord4 = false;
+var timeRecord4 = 0;
 appStart();
 function appStart() {
     document.addEventListener('keypress', onKeyPress);
     var btnPlayChannel1 = document.querySelector('#playChannel1');
     btnPlayChannel1.addEventListener('click', onPlayChannel1);
+    var btnRecord1 = document.querySelector('#record1');
+    btnRecord1.addEventListener('click', function (e) {
+        ifRecord1 = true;
+        timeRecord1 = e.timeStamp;
+    });
+    var btnStop1 = document.querySelector('#stop1');
+    btnStop1.addEventListener('click', function (e) {
+        ifRecord1 = false;
+    });
+    var btnPlayChannel2 = document.querySelector('#playChannel2');
+    btnPlayChannel2.addEventListener('click', onPlayChannel2);
+    var btnRecord2 = document.querySelector('#record2');
+    btnRecord2.addEventListener('click', function (e) {
+        ifRecord2 = true;
+        timeRecord2 = e.timeStamp;
+    });
+    var btnStop2 = document.querySelector('#stop2');
+    btnStop2.addEventListener('click', function (e) {
+        ifRecord2 = false;
+    });
+    var btnPlayChannel3 = document.querySelector('#playChannel3');
+    btnPlayChannel3.addEventListener('click', onPlayChannel3);
+    var btnRecord3 = document.querySelector('#record3');
+    btnRecord3.addEventListener('click', function (e) {
+        ifRecord3 = true;
+        timeRecord3 = e.timeStamp;
+    });
+    var btnStop3 = document.querySelector('#stop3');
+    btnStop3.addEventListener('click', function (e) {
+        ifRecord3 = false;
+    });
+    var btnPlayChannel4 = document.querySelector('#playChannel4');
+    btnPlayChannel4.addEventListener('click', onPlayChannel4);
+    var btnRecord4 = document.querySelector('#record4');
+    btnRecord4.addEventListener('click', function (e) {
+        ifRecord4 = true;
+        timeRecord4 = e.timeStamp;
+    });
+    var btnStop4 = document.querySelector('#stop4');
+    btnStop4.addEventListener('click', function (e) {
+        ifRecord4 = false;
+    });
     getAudioElements();
 }
 function onPlayChannel1() {
     channel1.forEach(function (sound) {
-        setTimeout(function () { return playSound(sound.key); }, sound.time);
+        setTimeout(function () { return playSound(sound.key); }, sound.newTime);
+    });
+}
+function onPlayChannel2() {
+    channel2.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.newTime);
+    });
+}
+function onPlayChannel3() {
+    channel3.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.newTime);
+    });
+}
+function onPlayChannel4() {
+    channel4.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.newTime);
     });
 }
 function getAudioElements() {
@@ -34,9 +102,31 @@ function getAudioElements() {
 function onKeyPress(ev) {
     var key = ev.key;
     var time = ev.timeStamp;
-    channel1.push({ key: key, time: time });
     playSound(key);
+    record(key, time);
     console.log(channel1);
+}
+function record(key, time) {
+    if (ifRecord1) {
+        var newTime = time - timeRecord1;
+        channel1.push({ key: key, newTime: newTime });
+        console.log(channel1);
+    }
+    if (ifRecord2) {
+        var newTime = time - timeRecord2;
+        channel2.push({ key: key, newTime: newTime });
+        console.log(channel1);
+    }
+    if (ifRecord3) {
+        var newTime = time - timeRecord3;
+        channel3.push({ key: key, newTime: newTime });
+        console.log(channel1);
+    }
+    if (ifRecord4) {
+        var newTime = time - timeRecord4;
+        channel4.push({ key: key, newTime: newTime });
+        console.log(channel1);
+    }
 }
 function playSound(key) {
     switch (key) {
--- Project2/index.ts
+++ Project2/index.ts
@@ -9,30 +9,110 @@ let snareSound: HTMLAudioElement;
 let boomSound: HTMLAudioElement;
 
 const channel1: any[] = [];
+const channel2: any[] = [];
+const channel3: any[] = [];
+const channel4: any[] = [];
 
 let ifRecord1: boolean = false;
+let timeRecord1: number = 0;
+
+let ifRecord2: boolean = false;
+let timeRecord2: number = 0;
+
+let ifRecord3: boolean = false;
+let timeRecord3: number = 0;
+
+let ifRecord4: boolean = false;
+let timeRecord4: number = 0;
 
 appStart();
 
 
 function appStart() {
+
     document.addEventListener('keypress', onKeyPress);
+
     const btnPlayChannel1 = document.querySelector('#playChannel1')
     btnPlayChannel1.addEventListener('click', onPlayChannel1);
     const btnRecord1 = document.querySelector('#record1')
     btnRecord1.addEventListener('click', e => {
-        ifRecord1 = true;
-    })
+            ifRecord1 = true;
+            timeRecord1 = e.timeStamp;
+    } );
+
+    const btnStop1 = document.querySelector('#stop1')
+    btnStop1.addEventListener('click', e => {
+        ifRecord1 = false;
+} );
+
+const btnPlayChannel2 = document.querySelector('#playChannel2')
+btnPlayChannel2.addEventListener('click', onPlayChannel2);
+const btnRecord2 = document.querySelector('#record2')
+btnRecord2.addEventListener('click', e => {
+        ifRecord2 = true;
+        timeRecord2 = e.timeStamp;
+} );
+
+const btnStop2 = document.querySelector('#stop2')
+btnStop2.addEventListener('click', e => {
+    ifRecord2 = false;
+} );
+
+const btnPlayChannel3 = document.querySelector('#playChannel3')
+btnPlayChannel3.addEventListener('click', onPlayChannel3);
+const btnRecord3 = document.querySelector('#record3')
+btnRecord3.addEventListener('click', e => {
+        ifRecord3 = true;
+        timeRecord3 = e.timeStamp;
+} );
+
+const btnStop3 = document.querySelector('#stop3')
+btnStop3.addEventListener('click', e => {
+    ifRecord3 = false;
+} );
+
+    const btnPlayChannel4 = document.querySelector('#playChannel4')
+    btnPlayChannel4.addEventListener('click', onPlayChannel4);
+    const btnRecord4 = document.querySelector('#record4')
+    btnRecord4.addEventListener('click', e => {
+            ifRecord4 = true;
+            timeRecord4 = e.timeStamp;
+    } );
+
+    const btnStop4 = document.querySelector('#stop4')
+    btnStop4.addEventListener('click', e => {
+        ifRecord4 = false;
+} );
+
     getAudioElements();
 }
-function onPlayChannel1(): void {
-    let prevTime = 0;
-    channel1.forEach(sound => {
-        const timeout = sound.time - prevTime;
-        setTimeout(() => playSound(sound.key), timeout)
-    })
 
+
+function onPlayChannel1(): void {
+    channel1.forEach(sound => {
+        setTimeout( () => playSound(sound.key), sound.newTime);
+    });
 }
+
+function onPlayChannel2(): void {
+    channel2.forEach(sound => {
+        setTimeout( () => playSound(sound.key), sound.newTime);
+    });
+}
+
+function onPlayChannel3(): void {
+    channel3.forEach(sound => {
+        setTimeout( () => playSound(sound.key), sound.newTime);
+    });
+}
+
+function onPlayChannel4(): void {
+    channel4.forEach(sound => {
+        setTimeout( () => playSound(sound.key), sound.newTime);
+    });
+}
+
+
 
 function getAudioElements(): void {
     kickSound = document.querySelector('[data-sound="kick"]');
@@ -51,12 +131,37 @@ function onKeyPress(ev: KeyboardEvent): void {
     const key = ev.key;
     const time = ev.timeStamp;
 
-    if(ifRecord1) {
-    channel1.push({ key, time })
+    playSound(key);
+    record(key, time);
+    console.log(channel1);
+}
+
+function record(key, time) {
+    if( ifRecord1 ) {
+        const newTime = time - timeRecord1;
+        channel1.push({key,newTime});
+        console.log(channel1);
     }
 
-    playSound(key);
-    console.log(channel1);
+    if( ifRecord2 ) {
+        const newTime = time - timeRecord2;
+        channel2.push({key,newTime});
+        console.log(channel1);
+    }
+
+    if( ifRecord3 ) {
+        const newTime = time - timeRecord3;
+        channel3.push({key,newTime});
+        console.log(channel1);
+    }
+
+    if( ifRecord4 ) {
+        const newTime = time - timeRecord4;
+        channel4.push({key,newTime});
+        console.log(channel1);
+    }
+
+
 }
 
 function playSound(key: string) {
